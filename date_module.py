9261546b4df3268c7e2266c2038ca8dc7f5c03cc
Julian Hamre <julian.hamre5@gmail.com>
2022-07-27 20:41:09 +0200

    Add get_full_format method
    The get_full_format method returns the date with month name and full year
    - Add get_full_format method
    - Set all variables and fitting methods to private
    - Date string attribute is now called ddmmyy

--- date_module.py
+++ date_module.py
@@ -7,49 +7,59 @@ Created on Thu Jun 16 10:47:18 2022
 """
 
 class date():
-    year_list = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-    leap_year_list = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    __year_list = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    __leap_year_list = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
     
-    def check_if_string(self):
-        tp = type(self.date)
+    def __check_if_string(self):
+        tp = type(self.__ddmmyy)
         if not (tp is str):
-            raise TypeError(f"in {self.date}, date must be a string, got {tp}")
+            raise TypeError(f"in {self.__ddmmyy}, date must be a string, got {tp}")
         
-    def check_length(self):
-        length = len(self.date)
+    def __check_length(self):
+        length = len(self.__ddmmyy)
         if not length == 6:
-            raise IndexError(f"in {self.date}, date must have 6 characters in the ddmmyy format, got {length} characters")
+            raise IndexError(f"in {self.__ddmmyy}, date must have 6 characters in the ddmmyy format, got {length} characters")
             
-    def correct_year_list(self):
-        if self.year_numb % 4 == 0:
-            return self.leap_year_list
+    def __correct_year_list(self):
+        if self.__year_numb % 4 == 0:
+            return self.__leap_year_list
         else:
-            return self.year_list
+            return self.__year_list
     
-    def check_day_and_month(self):
-        mn = self.month_numb 
-        dn = self.day_numb
+    def __check_day_and_month(self):
+        mn = self.__month_numb 
+        dn = self.__day_numb
         if not (mn >= 1 and mn <= 12):
-            raise ValueError(f"in {self.date}, month {mn} is invalid")
-        if not (dn >= 1 and dn <= self.correct_year_list()[mn - 1]):
-            raise ValueError(f"in {self.date}, day {dn} in month {mn} is invalid")
+            raise ValueError(f"in {self.__ddmmyy}, month {mn} is invalid")
+        if not (dn >= 1 and dn <= self.__correct_year_list()[mn - 1]):
+            raise ValueError(f"in {self.__ddmmyy}, day {dn} in month {mn} is invalid")
     
-    def __init__(self, date):
-        self.date = date
+    def __init__(self, ddmmyy):
+        self.__ddmmyy = ddmmyy
         
-        self.check_if_string()
-        self.check_length()
+        self.__check_if_string()
+        self.__check_length()
         
-        self.day_numb = int(date[0:2])
-        self.month_numb = int(date[2:4])
-        self.year_numb = int(date[4:6])
+        self.__day_numb = int(ddmmyy[0:2])
+        self.__month_numb = int(ddmmyy[2:4])
+        self.__year_numb = int(ddmmyy[4:6])
         
-        self.check_day_and_month()
+        self.__check_day_and_month()
         
+    def get_ddmmyy(self):
+        return self.__ddmmyy
+    
+    def get_full_format(self):
+        month_names = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+        year = self.__year_numb
+        if year < 10:
+            year = f"0{year}"
+        full_format = f"{month_names[self.__month_numb - 1]} {self.__day_numb}, 20{year}"
+        return full_format
         
     def date_to_day_number(self):
         year_value = 0
-        for i in range(0, self.year_numb):
+        for i in range(0, self.__year_numb):
             if i % 4 == 0:
                 year_value += 366
             else:
@@ -57,11 +67,11 @@ class date():
             
         month_value = 0
         month_counter = 0
-        for i in range(0, self.month_numb - 1):
-            month_value += self.correct_year_list()[month_counter]
+        for i in range(0, self.__month_numb - 1):
+            month_value += self.__correct_year_list()[month_counter]
             month_counter += 1
         
-        day_value = self.day_numb
+        day_value = self.__day_numb
         
         return year_value + month_value + day_value 
 
