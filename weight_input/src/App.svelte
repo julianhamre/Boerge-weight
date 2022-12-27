b643d25c9c1d1dcc15fffc20d72ce4f5d96314c9
Julian Hamre <julian.hamre5@gmail.com>
2022-12-27 20:24:28 +0100

    add svelte project named weight_input - add weight input - add date input

---
+++ weight_input/src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+    let raw_date = "";
+    let weight = 75;
+</script>
+
+<h1>New weight measurement</h1>
+
+<div class="select_container">
+    Select your weight: <input
+        type="number"
+        bind:value={weight}
+        min="0"
+        max="200"
+    /> kg
+</div>
+
+<div class="select_container">
+    Select the date: <input type="date" bind:value={raw_date} />
+</div>
